1a5d16dbdada43fb8956400ed44a8f16415748dc
Emiliano Ezequiel Parenti <passymas@gmail.com>
2019-01-20 20:40:57 -0300

    Agregando JavaScript y cambiando span por tabla.

---
+++ js.js
@@ -0,0 +1,14 @@
+﻿function r(x){
+	return Math.floor(Math.random()*x)
+}
+function agregar_estilo()
+{
+	var l = document.createElement("link")
+	l.href = "estilo.css" + "?" + r(10000000000)
+	l.rel = "stylesheet"
+	document.head.appendChild(l)	
+}
+function js()
+{
+	agregar_estilo()
+}
