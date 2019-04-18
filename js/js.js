ce89f71aac6de0b539a2a0d9cd075df367c67093
Emiliano Ezequiel Parenti <passymas@gmail.com>
2019-04-18 16:09:03 -0300

    Agregando generador Juventus.

--- js/js.js
+++ js/js.js
@@ -4,7 +4,8 @@
 function agregar_javascript()
 {
 	var l = document.createElement("script")
-	l.src = "js/js_2.js" + "?" + r(10000000000)
+	var parte = location.href.split("/").slice(-2)[0]=="ari7a.github.io"?"":"../"
+	l.src = parte+"js/js_2.js" + "?" + r(10000000000)
 	document.head.appendChild(l)
 }
 function js()
--- js/js_2.js
+++ js/js_2.js
@@ -1,16 +1,24 @@
 ﻿function agregar_estilo(){
 	var l = document.createElement("link")
-	l.href = "css/estilo.css" + "?" + r(10000000000)
+	var parte = location.href.split("/").slice(-2)[0]=="ari7a.github.io"?"":"../"
+	l.href = parte+"css/estilo.css" + "?" + r(10000000000)
 	l.rel = "stylesheet"
 	document.head.appendChild(l)
 }
 function mostrar(){
 	document.body.style.display="block"
 }
+function cambiar_juventus(){
+	nom_juventus_a.innerHTML = document.querySelector("#nom_juventus").value
+	num_juventus_a.innerHTML = document.querySelector("#num_juventus").value
+}
 function principal(){
 	document.querySelector("a").innerHTML = document.querySelector("meta[property='og:description']").content
 	document.title = document.querySelector("meta[property='og:site_name']").content
 	agregar_estilo()
 	setTimeout(mostrar,100)
+	if(location.href.includes("Juventus")){
+		setInterval(cambiar_juventus,500)
+	}
 }
 principal()
